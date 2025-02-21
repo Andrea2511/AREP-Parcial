c3c85fc4e54ecbe29fd0f16d9a9f103118c648fd
andrea.torres-t <andrea.torres-t@mail.escuelaing.edu.co>
2025-02-21 12:54:42 -0500

    feat: refactor HttpServer

--- src/main/java/co/edu/eci/arep/HttpServer.java
+++ src/main/java/co/edu/eci/arep/HttpServer.java
@@ -2,11 +2,13 @@ package co.edu.eci.arep;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.reflect.Array;
 import java.net.*;
 import java.io.*;
+import java.util.Arrays;
 
 public class HttpServer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
 
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
@@ -29,33 +31,62 @@ public class HttpServer {
 
             PrintWriter out = new PrintWriter(
                     clientSocket.getOutputStream(), true);
+
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(clientSocket.getInputStream()));
+
             String inputLine, outputLine;
-            while ((inputLine = in.readLine()) != null) {
+            StringBuffer response = new StringBuffer();
+
+
+            while ((inputLine = " " + in.readLine()) != null) {
+                response.append(inputLine);
                 System.out.println("Recibí: " + inputLine);
+
                 if (!in.ready()) {
                     break;
                 }
             }
-            outputLine = "HTTP/1.1 200 OK\r\n"
-                    + "Content-Type: text/html\r\n"
-                    + "\r\n"
-                    + "<!DOCTYPE html>\n"
-                    + "<html>\n"
-                    + "<head>\n"
-                    + "<meta charset=\"UTF-8\">\n"
-                    + "<title>Title of the document</title>\n"
-                    + "</head>\n"
-                    + "<body>\n"
-                    + "<h1>Mi propio mensaje</h1>\n"
-                    + "</body>\n"
-                    + "</html>\n";
+
+            String[] r = response.toString().split(" ");
+            System.out.println("response: " + Arrays.toString(response.toString().split(" ")));
+
+            String path = r[5];
+            String query = r[2];
+            System.out.println("path " + path);
+            System.out.println("query " + query);
+
+            String uri = path + query;
+
+            URI absolutePath = new URI(uri);
+            System.out.println("uri: " + uri);
+
+            outputLine = handleRequest(absolutePath);
             out.println(outputLine);
             out.close();
             in.close();
             clientSocket.close();
             serverSocket.close();
         }
+    }
+
+    private static String handleRequest(URI uri){
+
+        String root = "/resources";
+
+        return "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n"
+                + "<title>Title of the document</title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<h1>Mi propio mensaje</h1>\n"
+                + "</body>\n"
+                + "</html>\n";
+
     }
 }
